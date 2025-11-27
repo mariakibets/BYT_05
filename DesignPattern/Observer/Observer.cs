9e5870ea66e6886f785b7c6d6c74fc975174d16d
Mari Kibets <mariakibec7@Gmail.com>
2025-11-27 17:26:02 +0100

    Observer Design Pattern

--- DesignPattern/Observer/Observer.cs
+++ DesignPattern/Observer/Observer.cs
@@ -2,13 +2,37 @@
 
 namespace DesignPattern.Observer
 {
-    public class ConcreteObserver(string userName) : IObserver
+    public class ConcreteObserver : IObserver
     {
         // Your code goes here... make sure to add this features:
         //1. Allow to hold the observer's name (e.g. when user Adam wants to observe the subject)
+        private readonly string _userName;
+        
         //2. Creating the Observer
+        public ConcreteObserver(string userName)
+        {
+            _userName = userName;
+        }
+        
+        public string UserName => _userName;
+        
         //3. Registering the Observer with the Subject
+        public void AddSubscriber(ISubject subject)
+        {
+            subject.RegisterObserver(this);
+        }
+
         //4. Removing the Observer from the Subject
+        public void RemoveSubscriber(ISubject subject)
+        {
+            subject.RemoveObserver(this);
+        }
+        
         //5. Observer will get a notification from the Subject using the following Method
+        public void Update(string message)
+        {
+            Console.WriteLine($"Hello {_userName}, Product Availability changed to: {message}");
+        }
     }
+    
 }

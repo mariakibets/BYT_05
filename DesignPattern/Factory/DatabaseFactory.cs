7af15d5dd1cd61f2cccfdaf4e7d4c652844ee45f
Mari Kibets <mariakibec7@Gmail.com>
2025-11-27 17:25:35 +0100

    Factory Design Pattern

--- DesignPattern/Factory/DatabaseFactory.cs
+++ DesignPattern/Factory/DatabaseFactory.cs
@@ -2,9 +2,11 @@
 {
     public static class DatabaseFactory
     {
-        //public static IDatabase CreateDatabase(...)
-        //{
-        //  ...
-        //}
+        public static IDatabase CreateDatabase(DatabaseType databaseType)
+        {
+            if (databaseType == DatabaseType.Oracle) return new OracleDatabase();
+            else if (databaseType == DatabaseType.SqlServer) return new SqlServerDatabase();
+            else throw new ArgumentException("Unknown database type: " + databaseType);
+        }
     }
 }

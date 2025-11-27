453306399a45e6e4f6efcfa64453d2fc8df1a1db
Mari Kibets <mariakibec7@Gmail.com>
2025-11-27 17:24:56 +0100

    Adapter Design Pattern

--- DesignPattern/Adapter/EmployeeAdapter.cs
+++ DesignPattern/Adapter/EmployeeAdapter.cs
@@ -4,10 +4,24 @@ namespace DesignPattern.Adapter
     public class EmployeeAdapter : ITarget
     {
         private readonly BillingSystem thirdPartyBillingSystem = new();
+    
+        public void ProcessCompanySalary(string[,] employeesArray)
+        {
+            List<Employee> employeesList = new List<Employee>();
+            
+            int rows = employeesArray.GetLength(0);
+            int cols = employeesArray.GetLength(1);
 
-        //public void ProcessCompanySalary(...)
-        //{
-        //    ...
-        //}
+            for (int i = 0; i < rows; i++)
+            {
+                int id = int.Parse(employeesArray[i, 0]);
+                string name = employeesArray[i, 1];
+                string designation = employeesArray[i, 2];
+                decimal salary = decimal.Parse(employeesArray[i, 3]);
+                
+                employeesList.Add(new Employee(id, name, designation, salary));
+            }
+            thirdPartyBillingSystem.ProcessSalary(employeesList);
+        }
     }
 }
